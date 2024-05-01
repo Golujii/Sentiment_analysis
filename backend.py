311bfe37d3d10f552ec3e83ab8bd51927bf9f6d5
Golujii <145102005+Golujii@users.noreply.github.com>
2024-05-01 11:48:27 +0530

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,36 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import pandas as pd
+import plotly.express as px
+mymodel=SentimentIntensityAnalyzer()
+mymodel
+#Permission
+f=InstalledAppFlow.from_client_secrets_file('key.json',['https://www.googleapis.com/auth/spreadsheets'])
+cred=f.run_local_server(port=0)
+#Create a service
+service=build('Sheets','v4',credentials=cred).spreadsheets().values()
+#Retrieve Data from google sheets
+d=service.get(spreadsheetId='1YYscsfmgPwhF9zQMWCIt2uWBC4Q2rqLSMdcOtocop-E',range='A1:B').execute()
+mycolumns=d['values'][0]
+mydata=d['values'][1:]
+df=pd.DataFrame(data=mydata,columns=mycolumns)
+pos=0
+neg=0
+neu=0
+for i in range(0,len(df)):
+    k=df._get_value(i,'Review')
+    pred=mymodel.polarity_scores(k)
+    if(pred['compound']>0.5):
+        pos=pos+1
+    elif(pred['compound']<-0.5):
+        neg=neg+1
+    else:
+        neu=neu+1
+posper=(pos/len(df))*100
+negper=(neg/len(df))*100
+neuper=(neu/len(df))*100
+print("Positive:",posper)
+print("Negative:",negper)
+print("Neutral:",neuper)
+fig=px.pie(values=[posper,negper,neuper],names=['Positive','Negative','Neutral'])
